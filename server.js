900fa98244f7456f537cc27a094a49956cd8974b
waikinchoong <waikinchoong@qiu.edu.my>
2026-03-02 02:47:43 +0800

    Updated server to use Railway MySQL environment variables

--- server.js
+++ server.js
@@ -21,13 +21,7 @@ app.use(session({
 
 /* ================= DATABASE ================= */
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-})
+const db = mysql.createConnection(process.env.DATABASE_URL)
 
 db.connect(err => {
     if (err) {
